633bb2bde481f4fcfc8405bd0361751bbee8df65
Raul <raulfmansur@hotmail.com>
2020-05-27 23:00:03 -0300

    Layouts principais criados

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.api.controllers.PetController
@@ -14,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var notPossibleToLoad: LinearLayoutCompat
+    private lateinit var refreshButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,9 @@ class MainActivity : AppCompatActivity() {
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = PetListAdapter(emptyList())
+        notPossibleToLoad = findViewById(R.id.not_loaded)
+
+        refreshButton = findViewById(R.id.refresh_button)
 
         recyclerView = findViewById<RecyclerView>(R.id.pet_list).apply {
             setHasFixedSize(true)
@@ -28,23 +36,22 @@ class MainActivity : AppCompatActivity() {
             adapter = viewAdapter
         }
 
+        refreshButton.setOnClickListener{
+            notPossibleToLoad.visibility = View.GONE
+            loadList()
+        }
+        loadList()
+    }
+
+    private fun loadList(){
         val call = PetController.allPets()
         call.enqueue(object : Callback<List<Pet>> {
             override fun onResponse(call: Call<List<Pet>>?, response: Response<List<Pet>>?) {
-                print("Deu bom")
                 response!!.body()?.let { (viewAdapter as PetListAdapter).updateData(it) }
-/*                recyclerView.apply {
-                    val pets: List<Pet>? = response!!.body()
-                    viewAdapter = PetListAdapter(pets!!)
-                    setHasFixedSize(true)
-                    layoutManager = viewManager
-                    adapter = viewAdapter
-                }
-                */
             }
 
             override fun onFailure(call: Call<List<Pet>>?, t: Throwable?) {
-                print("Deu erro :(")
+                notPossibleToLoad.visibility = View.VISIBLE
             }
         })
     }
